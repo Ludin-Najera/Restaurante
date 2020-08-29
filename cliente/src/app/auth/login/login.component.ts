fdae5986fa9ee62fd881fdaceff7e429aca2277e
Ludin-Najera <69707485+Ludin-Najera@users.noreply.github.com>
2020-08-29 13:57:00 -0600

    login

--- cliente/src/app/auth/login/login.component.ts
+++ cliente/src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { UserI } from '../../models/user';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onLogin(form): void {
+    console.log('Login', form.value);
   }
 
 }
