ed73a8c3c2ff51c72cb69a6254020ea379ae2b27
Ludin-Najera <69707485+Ludin-Najera@users.noreply.github.com>
2020-09-11 23:07:43 -0600

    update

--- cliente/src/app/components/complementos2/complementos2.component.ts
+++ cliente/src/app/components/complementos2/complementos2.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Host, HostBinding } from '@angular/core';
+import { ComplementosService } from '../../services/inicio.service';
+import { complementos1 } from '../../models/complementos';
 
 @Component({
   selector: 'app-complementos2',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Complementos2Component implements OnInit {
 
-  constructor() { }
+  @HostBinding('class') classes = 'row';
 
-  ngOnInit(): void {
+  complementos: any = [];
+
+  constructor(private complementosservice: ComplementosService) { }
+
+  ngOnInit(){
+    this.actualizar();
   }
+
+  actualizar(){
+    this.complementosservice.getcomplementos().subscribe(
+      res => {
+        this.complementos = res;
+      },
+        
+      err => console.error(err)
+    );
+  }
+
+  eliminarcomplementos(id: string){
+    this.complementosservice.eliminarcomplementos(id).subscribe(
+      res => {
+        console.log(res)
+        this.actualizar();
+      },
+      err => console.log(err)
+    );
+  }
+
+  editarcomplementos(id: string){
+    console.log(id);
+  }
+
+
 
 }
