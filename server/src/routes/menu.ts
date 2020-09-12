c9f52b11dbbfbc75a2e42ef21fbc7abfec13d603
barruco <51138491+barruco@users.noreply.github.com>
2020-09-12 03:33:42 -0600

    API Rest
    Cambios API Rest

--- server/src/routes/menu.ts
+++ server/src/routes/menu.ts
@@ -16,11 +16,7 @@ class LogRoutes {
         this.router.get('/', MenuController.list);
         this.router.get('/:idmenu',MenuController.getone);
         this.router.post('/', MenuController.create);
-<<<<<<< HEAD
         this.router.put('/:idmenu',MenuController.update);
-=======
-        this.router.put('/:id',MenuController.update);
->>>>>>> 45849353a291353fef7466bba1b80933d42be9e4
         this.router.delete('/:idmenu',MenuController.delete);
     }
 
