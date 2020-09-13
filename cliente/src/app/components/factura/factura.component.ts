293277a7369d18de42b880ad50fe80bec171af6e
Ludin-Najera <69707485+Ludin-Najera@users.noreply.github.com>
2020-09-12 21:26:11 -0600

    update

--- cliente/src/app/components/factura/factura.component.ts
+++ cliente/src/app/components/factura/factura.component.ts
@@ -15,15 +15,6 @@ export class FacturaComponent implements OnInit {
   status:string[]=["Preparacion","Entrega","Finalizado"];
   sellista:string[]=[];
   
-  bebida:string[]=["Coca Cola","Sprite","Fanta Uva","Fanta Naranja","Coca Cola Zero","Agua Pura","Frezca","Fuze Tea Durazno","Fuze Tea Mango","Fuze Tea Manzana","Agua Mineral","Coca Cola Ligth"];
-  selbebida:string[]=[];
-
-  menu:string[]=["Combo Familia 1","Combo Familiar 2","Combo Familiar 3","Combo Familiar 4","Menu Pollo","Menu Pechuguitas","Menu desayuno 1","Menu desayuno 2","Menu de alitas"];
-  selmenu:string[]=[];
-
-  complemento:string[]=["Ensalada Verde","Ensalada de repollo","Papa al Horno","Pure de papa","Papa frita grande","Papa frita mediana","Papa frita pequeña","Helado","Pie de piña","Hershey pie","Aros de cebolla"];
-  selcomplemento:string[]=[];
- 
 
 factura: factura1 = {
     idfactura: 0,
